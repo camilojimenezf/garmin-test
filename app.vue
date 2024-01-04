8228f30b72c822ce02d180507d7cbfb186e96e1a
camilojimenezf <camilob270@gmail.com>
2024-01-04 09:04:11 -0300

    first composable to check bt devices connection

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen bg-gray-200">
+    <h1 class="text-4xl block font-bold text-blue-600">Welcome to Vue 3 with Tailwind!</h1>
+    <h2 class="text-2xl block font-bold text-blue-500">Is Connected: {{ isConnected }}</h2>
+    <h2 class="text-2xl font-bold text-blue-500 mb-5">Is Requesting: {{ isRequesting }}</h2>
+    <h2 class="text-2xl font-bold text-blue-500 mb-5">Has Bluetooth Availability: {{ hasBluetoothAvailability }}</h2>
+    <button class="bg-blue-500 mb-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      @click="getAvailableDevices()">
+      Get Available Bluetooth Devices
+    </button>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="getDevices()">
+      Get Current Devices
+    </button>
+  </div>
+</template>
+
+<script setup>
+const {
+  isConnected,
+  isRequesting,
+  hasBluetoothAvailability,
+  getAvailableDevices,
+  getDevices,
+} = useGarmin2();
+
+</script>
+
+<style scoped></style>
---
+++ composables/useGarmin2.js
@@ -0,0 +1,96 @@
+import { onMounted, onUnmounted } from "vue";
+
+const options = {
+  filters: [{ namePrefix: "Garmin" }],
+};
+
+export const useGarmin2 = () => {
+  const isRequesting = ref(false);
+  const isConnected = ref(false);
+  const hasBluetoothAvailability = ref(false);
+
+  const checkBluetoothAvailability = async () => {
+    try {
+      const availability = await navigator.bluetooth.getAvailability();
+      hasBluetoothAvailability.value = availability;
+      return availability;
+    } catch (err) {
+      alert(err.message);
+      console.log("err", err);
+    }
+  };
+
+  const getAvailableDevices = async () => {
+    console.log("requestDevice");
+    alert("requestDevice");
+    isRequesting.value = true;
+    try {
+      const response = await navigator.bluetooth.requestDevice(options);
+      console.log("response", response);
+      // TODO: validate device Gatt connection and handle unsupported device.
+      isConnected.value = true;
+      return response;
+    } catch (err) {
+      alert(err.message);
+      console.log("err", err);
+    }
+    isRequesting.value = false;
+  };
+
+  const connectDevice = async (device) => {
+    console.log("connectDevice");
+    alert("Connecting Device...");
+    isRequesting.value = true;
+    try {
+      const server = await device.gatt.connect();
+      console.log("server", server);
+      isConnected.value = true;
+      return server;
+    } catch (err) {
+      alert(err.message);
+      console.log("err", err);
+    }
+    isRequesting.value = false;
+  };
+
+  const getDevices = async () => {
+    console.log("getDevices");
+
+    isRequesting.value = true;
+    try {
+      const devices = await navigator.bluetooth.getDevices();
+      console.log("devices", devices);
+      devices.forEach(async (device) => {
+        if (device.name.includes("Garmin")) {
+          // TODO: validate device Gatt connection and handle unsupported device.
+          if (device.gatt.connected) {
+            isConnected.value = true;
+            return;
+          }
+          await connectDevice(device);
+        }
+      });
+      return devices;
+    } catch (err) {
+      alert(err.message);
+      console.log("err", err);
+    }
+    isRequesting.value = false;
+  };
+
+  onMounted(async () => {
+    console.log("onMounted");
+    const isAvailable = await checkBluetoothAvailability();
+    // TODO: handle case when is not available.
+    if (!isAvailable) return;
+    getDevices();
+  });
+
+  return {
+    isConnected,
+    isRequesting,
+    hasBluetoothAvailability,
+    getAvailableDevices,
+    getDevices,
+  };
+};
