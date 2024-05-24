fc60470647997ec18eaf444d4edd3f45b39a7b50
camilojimenezf <camilob270@gmail.com>
2024-05-24 14:37:43 -0400

    adjust interpolation steps

--- utils/interpolation.js
+++ utils/interpolation.js
@@ -1,7 +1,7 @@
 export function getStepsInterpolation(speed) {
-  if (speed < 0.5) return 2;
-  if (speed < 1) return 3;
-  if (speed < 3) return 5;
+  if (speed < 0.5) return 5;
+  if (speed < 1) return 5;
+  if (speed < 3) return 6;
   if (speed < 5) return 7;
   return 9;
 }
