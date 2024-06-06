2e190d2dddd4ce07e8d9eb1dfa3748d45c3a7f1a
camilojimenezf <camilob270@gmail.com>
2024-06-06 11:03:38 -0400

    add online supported

--- pages/BrowserSupport.vue
+++ pages/BrowserSupport.vue
@@ -5,6 +5,7 @@
       <p class="text-sm text-blue-700">Connection Supported: {{ connectionSupported }}</p>
       <p class="text-sm text-blue-700">ServiceWorker Supported: {{ serviceWorkerSupported }}</p>
       <p class="text-sm text-blue-700">Geolocation Supported: {{ geolocationSupported }}</p>
+      <p class="text-sm text-blue-700">Online Supported: {{ onlineSupported }}</p>
     </div>
   </div>
 </template>
@@ -15,17 +16,20 @@ import { computed } from 'vue';
 const connectionSupported = 'connection' in navigator;
 const serviceWorkerSupported = 'serviceWorker' in navigator;
 const geolocationSupported = 'geolocation' in navigator;
+const onlineSupported = 'onLine' in navigator;
 
 const isValidBrowser = computed(() => {
   const connectionSupported = 'connection' in navigator;
   const serviceWorkerSupported = 'serviceWorker' in navigator;
   const geolocationSupported = 'geolocation' in navigator;
+  const onlineSupported = 'onLine' in navigator;
 
   console.log('Connection supported:', connectionSupported);
   console.log('ServiceWorker supported:', serviceWorkerSupported);
   console.log('Geolocation supported:', geolocationSupported);
+  console.log('Online supported:', onlineSupported);
 
-  return connectionSupported && serviceWorkerSupported && geolocationSupported;
+  return connectionSupported && serviceWorkerSupported && geolocationSupported && onlineSupported;
 });
 
 </script>
